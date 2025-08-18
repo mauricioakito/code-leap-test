4f000d61f774c85bbbf9fbe88cc9ba4a17825284
Mauricio Akito <mauricio94jap@hotmail.com>
2025-08-18 10:56:17 -0300

    refact(Modal): Removed unused declaration

--- src/presentation/components/Modal/index.tsx
+++ src/presentation/components/Modal/index.tsx
@@ -1,11 +1,9 @@
 import {Welcome} from './Welcome'
-// import {Create} from './Create'
 import {Edit} from './Edit'
 import {Delete} from './Delete'
 
 export const Modal = {
   Welcome,
-  // Create,
   Edit,
   Delete
 }
