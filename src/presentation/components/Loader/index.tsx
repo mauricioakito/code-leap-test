91a40b88722d9a3a3734c4fc0d47b1c789c01ac6
Mauricio Akito <mauricio94jap@hotmail.com>
2025-08-18 10:55:59 -0300

    fix(Loader): Fixed loader image url path

--- src/presentation/components/Loader/index.tsx
+++ src/presentation/components/Loader/index.tsx
@@ -3,7 +3,7 @@ import styles from './Loader.module.scss'
 export const Loader = ({message}: {message: string}) => {
   return (
     <div className={styles.loader}>
-      <img className={styles.loaderGif} src='/loading.gif'/>
+      <img className={styles.loaderGif} src='/code-leap-test/loading.gif'/>
       <p className={styles.loaderMessage}>{message}</p>
     </div>
   )
