3ff3f326eeef82b6c9d8b2087a382071831888b0
Mauricio Akito <mauricio94jap@hotmail.com>
2025-08-18 08:49:15 -0300

    feat(Project): Added github deploy capability

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    resolve: {
+  base: "/code-leap-test/",
+  resolve: {
     alias: {
-      '@': '/src',
+      "@": "/src",
     },
   },
   define: {
-    '__BASE_URL__': JSON.stringify('https://dev.codeleap.co.uk/careers/'),
-  }
-})
+    __BASE_URL__: JSON.stringify("https://dev.codeleap.co.uk/careers/"),
+  },
+});
