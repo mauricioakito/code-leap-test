59df701726e4942f68dc679367aed831979368f2
Mauricio Akito <mauricio94jap@hotmail.com>
2025-08-25 10:59:57 -0300

    fix(Scroller): Added new fix for when there aren't results in the posts get request

--- src/presentation/components/Scroller/index.tsx
+++ src/presentation/components/Scroller/index.tsx
@@ -1,45 +1,48 @@
-import styles from './Scroller.module.scss';
-import { Form } from '../Form';
-import { Post } from '../Post';
-import { useStore } from '@/store/useStore';
-import { Loader } from '../Loader';
-import Portal from '../Portal';
-import { Modal } from '../Modal';
+import styles from "./Scroller.module.scss";
+import { Form } from "../Form";
+import { Post } from "../Post";
+import { useStore } from "@/store/useStore";
+import { Loader } from "../Loader";
+import Portal from "../Portal";
+import { Modal } from "../Modal";
 
 export const Scroller = () => {
-
-  const {posts, modalShow} = useStore()
+  const { posts, modalShow, hasRequestedPosts } = useStore();
 
   return (
     <div className={styles.scroller}>
       <div className={styles.header}>
         <span className={styles.headerTitle}>CodeLeap Network</span>
       </div>
-      <Form.CreateOrEdit requestType='create'/>
+      <Form.CreateOrEdit requestType="create" />
 
-      {posts.length === 0 ? 
-        <Loader message="Please wait while the posts are loaded..." /> :
+      {posts.length === 0 && !hasRequestedPosts ? (
+        <Loader message="Please wait while the posts are loaded..." />
+      ) : posts.length === 0 && hasRequestedPosts ? (
+        <p className={styles.results}>There are currently a total of 0 posts</p>
+      ) : (
         <div className={styles.postContainer}>
-          {posts.map((item) => <Post key={item.id} item={item}/>)}
+          {posts.map((item) => (
+            <Post key={item.id} item={item} />
+          ))}
         </div>
-      }
+      )}
 
       {modalShow.edit && (
         <Portal>
-          <div className='overlay'>
+          <div className="overlay">
             <Modal.Edit />
           </div>
         </Portal>
       )}
-      
+
       {modalShow.delete && (
         <Portal>
-          <div className='overlay'>
+          <div className="overlay">
             <Modal.Delete />
           </div>
         </Portal>
       )}
-
     </div>
-  )
-}
+  );
+};
